a0ba26b0900e766abbb377fb94b6873216676bed
Luis <pereira1214@gmail.com>
2021-09-21 15:00:08 -0400

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+ let task = document.querySelector('form')
+ task.addEventListener('submit', (e) => {
+  e.preventDefault()
+  taskToDo(document.getElementById("new-task-description").value) 
+  task.reset()
+
+  });
 });
+
+function taskToDo (todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement("button")
+  btn.addEventListener('click', deleteBtn)
+  btn.textContent = "x"
+  p.textContent = `${todo} `
+  p.appendChild(btn)
+  document.getElementById("list").appendChild(p)
+  p.addEventListener('click', displayDate)
+
+
+}
+
+function deleteBtn(event) {
+  event.target.parentNode.remove()
+}
+
+
+
